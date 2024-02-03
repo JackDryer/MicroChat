ac01027cbda543ff1f7de8ae5c798d0b692264d5
Jack Dryer <jackjbld@gmail.com>
2024-02-03 21:38:38 +0000

    Added a bunch of print statements so i can debug why nothing happens

--- microbitsgui.py
+++ microbitsgui.py
@@ -18,6 +18,7 @@ class SerialReaderProtocolRaw(Protocol):
 
     def data_received(self, data):
         """Called with snippets received from the serial port"""
+        print(data)
         self.tk_listener.after(0, self.tk_listener.on_data, data.decode())
 
 
@@ -44,12 +45,13 @@ class SerialReaderProtocolLine(LineReader):
                 self.to_receive -=1
             #if we jsut fininshed reciving a mesage, send it up to be read
             if self.to_receive ==0:
+                print(self.received)
                 self.handle_message("".join(self.received))
                 self.received = []
         except ValueError as e:
-            print("error resigning this message", e)
+            print("error receiving this message", e)
     def handle_message(self,message):
-        full_message = json.loads() #why operate on strings??, as lower levels kina needs it at the moment, im im not sure at this point how that code works ¯\_(ツ)_/¯
+        full_message = json.loads(message) #why operate on strings??, as lower levels kina needs it at the moment, im im not sure at this point how that code works ¯\_(ツ)_/¯
         if full_message["type"] == 4: #pain text message, used to demmo why it's insecure
             self.tk_listener.after(0, self.tk_listener.on_data, f'(Plaintext){full_message["username"]} >{full_message["message"]}')
         else:
@@ -72,7 +74,7 @@ class MainFrame(tk.Frame):
 class SendingBox(tk.Entry):
     def __init__(self,port:Serial,mainFrame:MainFrame, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.bind("<Return>",self.send)
+        self.bind("<Return>",self.send_plaintext)
         self.port = port
         self.grid(sticky="NSEW")
         self.mainFrame= mainFrame
@@ -81,14 +83,15 @@ class SendingBox(tk.Entry):
         message_bytes = message.replace(" ", "¬").encode("utf-8")
         frame_size = 14
         num_frames = (len(message_bytes)/frame_size).__ceil__()
+        print(message_bytes)
         self.port.write(str(num_frames).encode("utf-8")+b"\r\n")
         for i in range(num_frames):
             index = i*frame_size
             self.port.write(message_bytes[index:index+frame_size]+b"\r\n")
-    def send_plaintext(self):
+    def send_plaintext(self,_event):
         message = self.get()
         full_message = json.dumps({"type":4,"username":"others","message":message})
-        self.send_raw
+        self.send_raw(full_message)
         self.mainFrame.on_data("You> "+self.get())
         self.delete(0, 'end')
 if __name__ == '__main__':
