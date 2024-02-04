2ed737ac018d61249d2866e9d0158cc994a0872a
Jack Dryer <jackjbld@gmail.com>
2024-02-04 23:31:16 +0000

    Major refactor!!! Its not *quite* TCP, the 3 way handshake is only 2 ways… Currently untested, committing so more teste can be done

---
+++ layer4.py
@@ -0,0 +1,135 @@
+import threading
+import time
+
+LINES_PER_PACKET = 4
+MAX_LINE_LENGTH = 14
+
+class CONTROL:
+    MESSAGE_END = "\u0004"
+    HEADER = "\u0001"
+    PAYLOAD = "\u0002"
+    SYN = "\u0016"
+    ACK = "\u0006"
+    VALID_MESSAGES = {MESSAGE_END,HEADER,PAYLOAD,SYN,ACK}
+class Received_Message:
+    def __init__ (self,TCP_Handler):
+        self.packets =  []
+        self.current_packet = None
+        self.TCP_Handler = TCP_Handler
+
+    def next_packet (self,next_packet_number:int):
+        if self.current_packet is not None:
+            print(f"uh oh, something broke {self.current_packet_num=}")
+        self.current_packet = Packet(next_packet_number)
+
+    def add_payload(self,payload):
+        if self.current_packet is None:
+            return # ignore it, it'll time out and re-send
+        self.current_packet.add_payload(payload)
+        if self.current_packet.is_complete:
+            self.packets.append(self.current_packet.get_payload())
+            self.current_packet = None
+            self.send_ack(self.current_packet)
+    def send_ack(self,packet):
+        self.TCP_Handler.send_packet(Packet(self.current_packet.number+1),"ACK")
+    def add_failed_line():
+        pass # it already failed, lets just drop it 
+    def get_message(self):
+        return "".join(self.successful_packets[i] for i in sorted(self.successful_packet_nums))
+
+    def timeout(self):
+        time.sleep(0.5)
+        if not self.is_complete:
+            self.failed_packet_nums = set(range(1,self.num_packets_to_receive+1)).difference(self.successful_packet_nums)
+            print("Timed out!")
+            print("error receiving packets",self.failed_packet_nums)
+            print(f"{self.successful_packets=}")
+            self.handle_errors()
+
+        
+class Packet:
+    def __init__(self, number:int, type = "payload"):
+        self.number = number
+        self.payload = ""
+        self.lines = 0 # this should be marked with a trailer, but for now this'll do
+        self.type = type
+    def add_payload(self,payload):
+        self.payload += payload
+        self.lines+=1
+    @property
+    def is_complete(self) ->bool:
+        return len(self.lines) ==LINES_PER_PACKET
+    def get_payload(self):
+        return self.payload
+
+class TCP_Handler:
+    def __init__(self, sending_port,layer_6):
+        self.sending_port =sending_port
+        self.layer_6= layer_6
+        self.current_receiving_message = None
+    def receive_frame(self,line):
+        if not line or line[0] not in CONTROL.VALID_MESSAGES:
+            if self.current_receiving_message is not None:
+                self.current_receiving_message.add_failed_line()
+            return
+        if line[0] ==CONTROL.SYN:
+            if self.current_receiving_message: # 2 mesages at once, BAD!
+                raise Exception("Tried to handle 2 messages at once")
+            self.current_receiving_message = Received_Message(self)
+            self.send_packet(Packet(int(line[1:])+1,"ACK"))
+            Received_Message
+        match line[0]:
+            case CONTROL.HEADER:
+                self.current_receiving_message.next_packet(int(line[1:]))
+            case CONTROL.PAYLOAD:
+                self.current_receiving_message.add_payload(line[1:])
+            case CONTROL.MESSAGE_END:
+                self.send_packet(Packet(int(line[1:])+1,"ACK"))
+                self.layer_6.handle_message(self.current_receiving_message.get_message())
+                self.current_receiving_message = None
+            case CONTROL.RESPONSE:
+                self.receive_ack(int(line[1:]))
+                
+
+    def send_packet(self,packet:Packet):
+        if packet.type =="SYN":
+            self.sending_port.write((CONTROL.SYN+str(packet.number)+"\r\n").encode("utf-8"))
+        if packet.type =="ACK":
+            self.sending_port.write((CONTROL.ACK+str(packet.number)+"\r\n").encode("utf-8"))
+        if packet.type =="END":
+            self.sending_port.write((CONTROL.MESSAGE_END+str(packet.number)+"\r\n").encode("utf-8"))
+        if packet.type =="payload":
+            self.sending_port.write((CONTROL.HEADER+str(packet.number)+"\r\n").encode("utf-8"))
+            for i in range(LINES_PER_PACKET-1):
+                index = i*MAX_LINE_LENGTH
+                self.sending_port.write((CONTROL.PAYLOAD+packet.get_payload()[index:index+MAX_LINE_LENGTH]+"\r\n").encode("utf-8"))
+        self.timeout = threading.Thread(target=self.timeout_send,args=(self.current_sending_packet.number,),daemon=True)
+    def timeout_send(self,number):
+        time.sleep(0.5)
+        if self.acknowledged == number: #could be a <= but were aking all packets so not an issue 
+            self.send_packet(self.current_sending_packet)
+    def receive_ack(self,number):
+        self.acknowledged = number
+        packet_size = MAX_LINE_LENGTH*LINES_PER_PACKET
+        index = (self.acknowledged-1)*packet_size # 1 indexed
+        payload = self.text_to_send[index:index+packet_size]
+        if payload:
+            self.current_sending_packet = Packet(number)
+            self.current_sending_packet.add_payload(payload)
+            self.send_packet(self.current_sending_packet)
+        else:
+            #finished sending message, close
+            if self.text_to_send:
+                self.current_sending_packet = Packet(number, "END")
+                self.text_to_send = ""
+                self.send_packet(self.current_sending_packet)
+            # if the text to send is already gone, the connection is now closed
+
+    def send(self,text):
+        self.text_to_send  = text
+        self.start_sending()
+    def start_sending(self):
+        self.acknowledged = 0
+        self.current_sending_packet = Packet(0,"SYN")
+        self.send_packet(self.current_sending_packet)
+
--- microbitsgui.py
+++ microbitsgui.py
@@ -2,24 +2,11 @@ import tkinter as tk
 
 from serial import Serial
 from serial.threaded import ReaderThread, Protocol, LineReader
-
-import threading
-import time
+from layer4 import TCP_Handler
 
 import json
 
 from microbitsmodule import get_micro
-
-username = ""
-class CONTROL:
-    MESSAGE_START = "\u0091"
-    MESSAGE_END = "\u0004"
-    HEADER = "\u0001"
-    PAYLOAD = "\u0002"
-    REQUEST = "\u0005"
-    RESPONSE = "\u0006"
-
-    VALID_MESSAGES = {MESSAGE_START,MESSAGE_END,HEADER,PAYLOAD,REQUEST,RESPONSE}
 
 
 class SerialReaderProtocolRaw(Protocol):
@@ -34,150 +21,27 @@ class SerialReaderProtocolRaw(Protocol):
     def data_received(self, data):
         """Called with snippets received from the serial port"""
         self.tk_listener.after(0, self.tk_listener.on_data, str(data))
- 
-LINES_PER_PACKET = 4
-MAX_LINE_LENGTH = 14
+
 class SerialReaderProtocolLine(LineReader): #layers 1  and 2
-    tk_listener = None
+    tcp_connection = None
     TERMINATOR = b'\r\n'
-    def __init__(self) -> None:
-        super().__init__()
-        self.reset_message()
+
     def connection_made(self, transport):
         """Called when reader thread is started"""
-        if self.tk_listener is None:
+        if self.tcp_connection is None:
             raise Exception("tk_listener must be set before connecting to the socket!")
         super().connection_made(transport)
         print("Connected, ready to receive data...")
 
     def handle_line(self, line:str): # so this code is out of our control and line will always be a str
+        '''this is layer 2 where we receive frames (in this case its lines), 
+        we could apply a hamming code here, but in testing errors within frames seem fairly rare'''
         line = line.strip()#so long \r\n
         #print(f"{self.current_packet_num=}")
-        if not line or line[0] not in CONTROL.VALID_MESSAGES: #python is just english
-            self.current_packet.add_failed_line()
-            return
-        if line[0] ==CONTROL.MESSAGE_START:
-                if self.current_message: # 2 mesages at once, BAD!
-                    raise Exception("Tried to handle 2 messages at once")
-                self.current_message = Message(line[1:])
-        elif self.current_message:
-            self.current_message = Message()
-        match line[0]:
-            case CONTROL.HEADER:
-                self.current_message.next_packet(int(line[1:]))
-            case CONTROL.PAYLOAD:
-                self.current_message.add_payload(line[1:])
-            case CONTROL.REQUEST:
-                pass
-            case CONTROL.RESPONSE:
-                self.current_message.add_correction(line[1:])
-        if self.current_message.is_complete:
-            self.handle_message(self.current_message.get_message())
+        '''here we are moving up to layer 4'''
+        self.tcp_connection.receive_frame(line)
 
-    def handle_message(self,message):
-        full_message = json.loads(message) #why operate on strings??, as lower levels kina needs it at the moment, im im not sure at this point how that code works ¯\_(ツ)_/¯
-        if full_message["type"] == 4: #pain text message, used to demmo why it's insecure
-            # Execute our callback in tk
-            self.tk_listener.after(0, self.tk_listener.on_data, f'(Plaintext){full_message["username"]} >{full_message["message"]}')
-        else:
-            print("unknown message type received", full_message)
-
-class Message:
-    def __init__ (self,num_packets_to_receive = None):
-        timeout = threading.Thread(daemon= True,target=self.timeout)
-        timeout.start()
-        if num_packets_to_receive is None:
-            self.num_packets_to_receive = None # we're gonna time out and then correct 
-        try:
-            self.num_packets_to_receive = int(num_packets_to_receive)
-        except ValueError as e:
-            print("error getting number packets", e)
-            self.num_packets_to_receive = None
-        self.successful_packet_nums = set()
-        self.failed_packet_nums=set()
-        self.successful_packets= {}
-        self.current_packet_num = 0
-        self.current_packet = None
-        self.handing_errors = False
-
-    def next_packet (self,next_packet_number:int):
-        if self.current_packet is not None:
-            print(f"uh oh, dropped packet! {self.current_packet_num=}")
-            self.failed_packet_nums.add(self.current_packet_num)
-        self.current_packet_num+=1
-        if next_packet_number !=self.current_packet_num:
-            print(f"Oh no, we skipped a packet! {self.current_packet_num =}, {next_packet_number=}")
-            self.failed_packet_nums.add(self.current_packet_num)
-            self.current_packet_num = next_packet_number
-
-    def add_payload(self,payload):
-        if self.current_packet is None:
-            self.current_packet_num+=1
-            self.failed_packet_nums.add(self.current_packet_num)
-            return
-        self.current_packet.add_payload(payload)
-        if self.current_packet.is_complete:
-            self.successful_packets[self.current_packet_num] = self.current_packet.get_payload()
-            self.current_packet = None
-            self.handle_errors_if_required()
-    
-    def handle_errors_if_required(self):
-        if self.required_to_handle_errors:
-            self.handle_errors()
-    def handle_errors (self):
-        print(f"should handle{self.failed_packet_nums}")
-    
-    @property
-    def required_to_handle_errors(self):
-        if self.handing_errors:
-            return self.has_errors
-        else: return False
-        self.failed_packet_nums.difference_update(self.successful_packet_nums)
-        return self.error_correcting or self.current_packet_num ==self.num_packets_to_receive
-    
-    @property
-    def has_errors(self):
-        if self.num_packets_to_receive is None:
-            return True
-        return not self.is_complete
-
-    @property
-    def is_complete(self) ->bool:
-        return len(self.successful_packet_nums)!= self.num_packets_to_receive
-    
-    def get_message(self):
-        return "".join(self.successful_packets[i] for i in sorted(self.successful_packet_nums))
-
-    def timeout(self):
-        time.sleep(0.5)
-        if not self.is_complete:
-            self.failed_packet_nums = set(range(1,self.num_packets_to_receive+1)).difference(self.successful_packet_nums)
-            print("Timed out!")
-            print("error receiving packets",self.failed_packet_nums)
-            print(f"{self.successful_packets=}")
-            self.handle_errors()
-
-            if : #ok so we just handled "all" packets, are there any errors
-                if self.failed_packet_nums:
-                    self.error_correcting = True
-                    self.num_packets_to_receive = len(self.failed_packet_nums)
-                    print("error receiving packets",self.failed_packet_nums)
-                    print(f"{self.successful_packets=}")
-
-        
-class Packet:
-    def __init__(self, number:int):
-        self.number = number
-        self.LINES = []
-    def add_payload(self,payload):
-        self.LINES.append([payload])
-    @property
-    def is_complete(self) ->bool:
-        return len(self.LINES) ==LINES_PER_PACKET
-    def get_payload(self):
-        return "".join(self.current_packet)
 class MainFrame(tk.Frame):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.listbox = tk.Listbox(self)#
@@ -188,32 +52,28 @@ class MainFrame(tk.Frame):
         self.listbox.insert(tk.END, data)
         self.listbox.see("end")
 
+class ChatStream:
+    def __init__(self,tk_listener) -> None:
+        self.tk_listener = tk_listener
+    def handle_message(self,message):
+        full_message = json.loads(message) #why operate on strings??, as lower levels kina needs it at the moment, im im not sure at this point how that code works ¯\_(ツ)_/¯
+        if full_message["type"] == 4: #pain text message, used to demmo why it's insecure
+            # Execute our callback in tk
+            self.tk_listener.after(0, self.tk_listener.on_data, f'(Plaintext){full_message["username"]} >{full_message["message"]}')
+        else:
+            print("unknown message type received", full_message)
 class SendingBox(tk.Entry):
-    def __init__(self,port:Serial,mainFrame:MainFrame,username, *args, **kwargs):
+    def __init__(self,tcp_connection,mainFrame:MainFrame,username, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bind("<Return>",self.send_plaintext)
-        self.port = port
+        self.tcp_connection = tcp_connection
         self.mainFrame= mainFrame
         self.username = username
     
-    def send_packet(self,number,packet):
-        self.port.write(str(number).encode("utf-8")+b"\r\n")
-        for i in range(LINES_PER_PACKET-1):
-            index = i*MAX_LINE_LENGTH
-            self.port.write(b'#'+packet[index:index+MAX_LINE_LENGTH]+b"\r\n")
-    def send_raw(self, message:str):
-        #message_bytes = message.replace(" ", "¬").encode("utf-8")
-        message_bytes = message.encode("utf-8")
-        num_frames = (len(message_bytes)/MAX_LINE_LENGTH).__ceil__()
-        num_packets = (num_frames/(LINES_PER_PACKET-1)).__ceil__() #-1 as the first line is the packet number
-        self.port.write(str(num_packets).encode("utf-8")+b"\r\n")
-        for i in range(num_packets):
-            index = i*MAX_LINE_LENGTH*(LINES_PER_PACKET-1)
-            self.send_packet(i+1,message_bytes[index:])
     def send_plaintext(self,_event):
         message = self.get()
         full_message = json.dumps({"type":4,"username":self.username.get(),"message":message})
-        self.send_raw(full_message)
+        self.tcp_connection.send(full_message)
         self.mainFrame.on_data("You> "+self.get())
         self.delete(0, 'end')
 class Username(tk.Entry):
@@ -229,17 +89,19 @@ if __name__ == '__main__':
     app.columnconfigure(0,weight=1)
     app.rowconfigure(1,weight=1)
     main_frame = MainFrame()
-    # Set listener to our reader
-    SerialReaderProtocolRaw.tk_listener = main_frame
     # Initiate serial port
     serial_port = get_micro()
-    box = SendingBox(serial_port,main_frame,username)
+    layer_6 = ChatStream(main_frame)
+    layer_4 = TCP_Handler(serial_port,layer_6)
+    # Set listener to our reader
+    SerialReaderProtocolLine.tcp_connection =  layer_4
+    sending_box = SendingBox(layer_4,main_frame,username)
     # Initiate ReaderThread
-    reader = ReaderThread(serial_port, SerialReaderProtocolRaw)
+    reader = ReaderThread(serial_port, SerialReaderProtocolLine)
     #build actual app
     username.grid(sticky="NSEW")
     main_frame.grid(sticky="NSEW",columnspan=2)
-    box.grid(sticky="NSEW",columnspan=2)
+    sending_box.grid(sticky="NSEW",columnspan=2)
     # Start reader
     reader.start()
     app.mainloop()
