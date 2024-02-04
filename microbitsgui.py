802cecf57446540d5523b8833e532cef44c4ee9f
Jack Dryer <jackjbld@gmail.com>
2024-02-04 00:49:30 +0000

    added usernames

--- microbitsgui.py
+++ microbitsgui.py
@@ -7,6 +7,7 @@ import json
 
 from microbitsmodule import get_micro
 
+username = ""
 class SerialReaderProtocolRaw(Protocol):
     tk_listener = None
 
@@ -26,6 +27,7 @@ class SerialReaderProtocolLine(LineReader):
     TERMINATOR = b'\r\n'
     to_receive = 0
     received = []
+
     def connection_made(self, transport):
         """Called when reader thread is started"""
         if self.tk_listener is None:
@@ -63,19 +65,17 @@ class MainFrame(tk.Frame):
         self.columnconfigure(0,weight=1)
         self.rowconfigure(0,weight=1)
         self.listbox.grid(sticky="NSEW")
-        self.grid(sticky="NSEW")
-
     def on_data(self, data):
         self.listbox.insert(tk.END, data)
         self.listbox.see("end")
 
 class SendingBox(tk.Entry):
-    def __init__(self,port:Serial,mainFrame:MainFrame, *args, **kwargs):
+    def __init__(self,port:Serial,mainFrame:MainFrame,username, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.bind("<Return>",self.send_plaintext)
         self.port = port
-        self.grid(sticky="NSEW")
         self.mainFrame= mainFrame
+        self.username = username
 
     def send_raw(self, message:str):
         message_bytes = message.replace(" ", "¬").encode("utf-8")
@@ -87,23 +87,34 @@ class SendingBox(tk.Entry):
             self.port.write(message_bytes[index:index+frame_size]+b"\r\n")
     def send_plaintext(self,_event):
         message = self.get()
-        full_message = json.dumps({"type":4,"username":"others","message":message})
+        full_message = json.dumps({"type":4,"username":self.username.get(),"message":message})
         self.send_raw(full_message)
         self.mainFrame.on_data("You> "+self.get())
         self.delete(0, 'end')
+class Username(tk.Entry):
+    def __init__(self,name,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.insert(tk.END,name)
+        self.old_name = name
+        
 if __name__ == '__main__':
+    username = input("Enter username:")
     app = tk.Tk()
+    username =Username(username)
     app.columnconfigure(0,weight=1)
-    app.rowconfigure(0,weight=1)
+    app.rowconfigure(1,weight=1)
     main_frame = MainFrame()
     # Set listener to our reader
     SerialReaderProtocolLine.tk_listener = main_frame
     # Initiate serial port
     serial_port = get_micro()
-    box = SendingBox(serial_port,main_frame)
+    box = SendingBox(serial_port,main_frame,username)
     # Initiate ReaderThread
     reader = ReaderThread(serial_port, SerialReaderProtocolLine)
+    #build actual app
+    username.grid(sticky="NSEW")
+    main_frame.grid(sticky="NSEW",columnspan=2)
+    box.grid(sticky="NSEW",columnspan=2)
     # Start reader
     reader.start()
-
     app.mainloop()
